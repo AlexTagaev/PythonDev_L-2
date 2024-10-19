15efb717960a54a17ea8c2397cc68863b34b17b0
Alexandr Tagaev <136466904+AlexTagaev@users.noreply.github.com>
2024-10-19 11:41:20 +0300

    Add files via upload

--- borndayforewer.py
+++ borndayforewer.py
@@ -2,6 +2,6 @@ born_year = None
 day_born = None
 while born_year != 1799:
     born_year = int(input('Введите год рождения А.С.Пушкина: '))
-while day_born != '6 июня':
-    day_born = input('Введите дату рождения А.С.Пушкина: ')
+while day_born != '06.06':
+    day_born = input('Введите дату рождения А.С.Пушкина (в формате dd.mm): ')
 print('Верно!')
